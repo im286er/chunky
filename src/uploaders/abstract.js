6a4dd485f70407327d613602c781369a2d1887f6
Phil Parsons <phil.m.parsons@gmail.com>
2015-02-22 09:33:40 +0000

    Start extracing uploaders

---
+++ src/uploaders/abstract.js
@@ -0,0 +1,105 @@
+(function () {
+  'use strict';
+
+  var AbstractUploader = function () {};
+
+  AbstractUploader.prototype = {
+    // ##Uploader states
+    _states: {
+      IDLE: 0, // The upload is not ready to start
+      READY: 1, // The request is setup ready to start the upload
+      UPLOADING: 2, // The upload is in progress
+      PAUSED: 3, // The upload has been paused
+      COMPLETE: 4, // The upload has completed
+      ERROR: 5 // The upload failed
+    },
+
+    // Gets the file upload status then start the upload.
+    //
+    // The request to the `status` endpoint should return a json object with
+    // the filesize attribute containing the size in bytes of the file on
+    // the server. If the file does not exist then 0 should be returned.
+    start: function () {
+      var fn = function () {
+        this.state = this._states.UPLOADING;
+        this.upload();
+      }.bind(this);
+
+      // Status of file upload has already been checked so start upload
+      if (this.state === this._states.READY) {
+        return fn();
+      }
+
+      // Check the status before starting
+      this.checkStatus(fn);
+    },
+
+    uploadChunk: function (chunk, callback) {
+      var xhr = new XMLHttpRequest
+        , upload = xhr.upload
+        , that = this;
+
+      xhr.open('POST', this.requestURL('upload'), true);
+      xhr.onload = function () {
+        if (this.status === 200) {
+          callback.call(that, JSON.parse(this.responseText));
+        }
+        else {
+          that.onError();
+        }
+      };
+      xhr.onerror = that.onError.bind(that);
+
+      xhr.send(chunk);
+      return xhr;
+    },
+
+    pause: function () {
+      this.state = this._states.PAUSED;
+      this.pauseUpload;
+    },
+
+    // Returns true if this uploader is actively uploading
+    isUploading: function () {
+      return this.state === this._states.UPLOADING;
+    },
+
+    // Returns true if this uploader is paused
+    isPaused: function () {
+      return this.state === this._states.PAUSED;
+    },
+
+    // Get the endpoint url with the files name appended
+    requestURL: function (endpoint) {
+      var url = this.settings.endpoints[endpoint];
+
+      if (url.indexOf('?') === -1) {
+        url += '?';
+      }
+      else {
+        url += '&';
+      }
+
+      // XXX Need to make this more generic with helper to get addtional
+      // parameters for the url
+      return url + 'filename=' + this.file.name;
+    },
+
+    // Default error handler,
+    onError: function () {
+      if (typeof this.settings.onerror === 'function') {
+        this.settings.onerror.call(this);
+      }
+    }
+
+    // Methods to be implemented in sub-classes
+    // ---
+    //
+    // checkStatus(callback) => XMLHttpRequest
+    // upload() => XMLHttpRequest
+    // pauseUpload() => void
+  };
+
+  // XXX Make AMD compatible
+  chunky._Uploader_ = AbstractUploader;
+}());
---
+++ src/uploaders/serial.js
@@ -0,0 +1,104 @@
+(function () {
+  'use strict';
+
+  var SerialUploader = function (file, settings) {
+    this.settings = settings;
+    this.file = file;
+    this.state = this._states.IDLE;
+    this.failedChunks = 0; // Track failed chunk uploads
+    this._upload;
+  };
+
+  // XXX Add an extends method
+  var sp = SerialUploader.prototype = new chunky._Uploader_;
+
+  // Check the status of a file upload by retrieving the size of the file on
+  // the server
+  sp.checkStatus = function (callback) {
+    var xhr = new XMLHttpRequest
+      , that = this;
+
+    xhr.open('GET', this.requestURL('status'), true);
+    xhr.onload = function () {
+      var status;
+
+      // upload parts
+      try {
+        // Attempt to get the file size from the json response body
+        status = JSON.parse(this.responseText);
+        that.chunkOffset = +status.filesize || 0;
+      }
+      catch (ex) {
+        that.chunkOffset = 0;
+      }
+
+      that.state = that._states.READY;
+
+      if (typeof callback === 'function') {
+        callback.call(that);
+      }
+    };
+
+    xhr.send();
+  };
+
+  sp.upload = function () {
+    var chunkTo = this.chunkOffset + this.settings.chunkSize
+      , chunk = this.file.slice(this.chunkOffset, chunkTo)
+      , that = this;
+
+    this.chunkOffset = chunkTo;
+    this._upload = this.uploadChunk(chunk, function (result) {
+      // Run callback for chunk completion if set
+      if (typeof that.settings.onchunkcomplete === 'function') {
+        that.settings.onchunkcomplete.call(that);
+      }
+
+      if (that.state === this._states.UPLOADING) {
+        // Upload next chunk or set as complete and finish
+        if (chunkTo < that.file.size) {
+          that.uploadChunk();
+        }
+        else {
+          that.state = this._states.COMPLETE;
+
+          if (typeof that.settings.oncomplete === 'function') {
+            that.settings.oncomplete.call(this);
+          }
+        }
+      }
+    });
+  };
+
+  sp.pauseUpload = function () {
+    if (
+      !this.settings.graceful &&
+      this._upload &&
+      typeof this._upload.abort === 'function'
+    ) {
+      this._upload.abort();
+    }
+  };
+
+  // Allows settings.attempts, which default to 1 if not set,  consecutive
+  // attempts before calling error handler
+  sp.onError = function () {
+    this.failedChunks += 1;
+
+    // Three failed attempts for the same chunk then abort and set state
+    if (this.failedChunks === (this.settings.attempts || 1)) {
+      this.state = this._states.ERROR;
+
+      if (typeof this.settings.onerror === 'function') {
+        this.settings.onerror.call(this);
+      }
+    }
+    // Retry the last chunk upload
+    else {
+      this.uploadChunk();
+    }
+  };
+
+  // XXX Make AMD compatible
+  chunky.uploaders.SerialUploader = SerialUploader;
+}());
